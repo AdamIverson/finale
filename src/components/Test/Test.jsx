17c25e8e6c7c8c8c23ee8ab63c0c9d9356b257ee
Adam Iverson <aciverso@gmail.com>
2022-05-16 17:21:31 -0500

    test.js

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Test() {
+  const testStore = useSelector((store) => store.setTest);
+
   return (
-    <h1>SUCCESSFUL TEST</h1>
-  )
+    <div>
+      <h1>SUCCESSFUL TEST</h1>
+      <ul>
+        {testStore.map((unit) => {
+          return (
+            <li key={unit.id}>
+              {unit.testData}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Test;
